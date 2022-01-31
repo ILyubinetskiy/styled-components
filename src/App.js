430dbdeb0a113d7489f5d0740bfbec275df718fa
Ivan_Lyubinetskiye <ILyubinetskiy@beeline.ru>
2022-01-31 16:37:52 +0300

    ref

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StoreProvider, useStore } from "webpack_module_federation/store";
 
 const App = () => {
-  const { users } = useStore();
+  const [localUsers, setLocalUsers] = useState([])
+  const { users, onIncrement, value } = useStore();
+  
+  useEffect(() => {
+    setLocalUsers([
+      ...users
+    ])
+  }, [users]);
 
   return (
-    <ul style={{
-      backgroundColor: "cyan"
-    }}>
-      {users.map(user => (<li key={user.id}>{user.name}</li>))}
-    </ul>
+    <>
+      <div>{value}</div>
+      <button onClick={onIncrement}>Click</button>
+      <ul style={{
+        backgroundColor: "cyan"
+      }}>
+        {localUsers.map(user => (<li key={user.id}>{user.name}</li>))}
+      </ul>
+    </>
   )
 }
 
