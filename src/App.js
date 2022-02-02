52cdb1b1db95a17fd906c48795bfe1a8c1cd65af
Ivan_Lyubinetskiye <ILyubinetskiy@beeline.ru>
2022-02-02 12:08:22 +0300

    ref

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StoreProvider, useStore } from "webpack_module_federation/store";
+import { useStore } from "webpack_module_federation/store";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const App = () => {
   const [localUsers, setLocalUsers] = useState([])
   const { users, onIncrement, value } = useStore();
-  
+
   useEffect(() => {
     setLocalUsers([
       ...users
@@ -12,15 +14,25 @@ const App = () => {
   }, [users]);
 
   return (
-    <>
+    <Box
+      sx={{
+        backgroundColor: 'primary.main',
+        display: 'flex',
+        'text-align': 'center'
+      }}
+    >
       <div>{value}</div>
-      <button onClick={onIncrement}>Click</button>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={onIncrement}
+      >Click</Button>
       <ul style={{
         backgroundColor: "cyan"
       }}>
         {localUsers.map(user => (<li key={user.id}>{user.name}</li>))}
       </ul>
-    </>
+    </Box>
   )
 }
 
